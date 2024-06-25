57318ea263cccd6fdc3b4b0419cd4eacd278057f
osama seid <osaseid1@gmail.com>
2024-06-25 12:28:10 +0300

    guess the secret nmber game

--- python_introduction/guess_secretN.py
+++ python_introduction/guess_secretN.py
@@ -2,25 +2,28 @@ import random
 
 # Generate a secret number
 secret_number = random.randint(1, 10)
+#initializinng guess counte
+guess_counter = 0
 
+while True:
 # Get user's guess
-guess = int(input("Guess the number (1-10): "))
-guss = +1
+   guess = int(input("Guess the number (1-10): "))
+   guss_counter = +1
 
-# Match the guess
-match guess:
-   
-    case number if number > secret_number:
-        print("Oops, your guess is a bit high. Try again!")
+# Match the guess 
+   match guess:
+       case number if number > secret_number:
+          print("Oops, your guess is a bit high. Try again!")
         
-    case _:
-        print("Nope, your guess is a bit low. Give it another shot!")
-    case secret_number:
-        print("Congratulations, you guessed it!")
+       case _:
+          print("Nope, your guess is a bit low. Give it another shot!")
+       case secret_number:
+          print("Congratulations, you guessed it!")
 
 # Offer to play again
-play_again = input("Do you want to play again? (y/n): ")
-if play_again.lower() == "y":
-    print("Let's play again!")
-else:
-    print("Thanks for playing! Goodbye.")
+   play_again = input("Do you want to play again? (y/n): ")
+   if play_again.lower() == "y":
+          print("Let's play again!")
+   else:
+        print("Thanks for playing! Goodbye.")
+        break
