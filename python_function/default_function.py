cab004e52cc34750025d7cb5f4e1866fe497ea62
osama seid <osaseid1@gmail.com>
2024-07-03 21:29:41 +0300

    greeting

--- python_function/default_function.py
+++ python_function/default_function.py
@@ -1,6 +1,5 @@
 # Function definition with default value
 def greet(name="World"):
-     """Prints a greeting message."""
      print(f"Hello, {name}!")
 greet() 
-greet("Alice")  
+greet("osama")  
