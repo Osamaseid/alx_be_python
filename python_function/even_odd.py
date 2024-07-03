7b866b1dd88de192eaf3fd82d349aafa6f2bfd05
osama seid <osaseid1@gmail.com>
2024-07-03 21:49:42 +0300

    even odd funnction

---
+++ python_function/even_odd.py
@@ -0,0 +1,8 @@
+num = float(input("Enter the number:"))
+
+def even_odd(num):
+    if num % 2 == 0:
+        print("your number is even")
+    else:
+        print("yor number is odd")
+print(even_odd(num))
