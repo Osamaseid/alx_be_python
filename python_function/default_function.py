0a4ad08f1ad3b3d7f58fa2cd4082692a35682163
osama seid <osaseid1@gmail.com>
2024-07-03 11:29:59 +0300

    return value

--- python_function/default_function.py
+++ python_function/default_function.py
@@ -2,5 +2,5 @@
 def greet(name="World"):
      """Prints a greeting message."""
      print(f"Hello, {name}!")
-greet()  # Output: Hello, World!
-greet("Alice")  # Output: Hello, Alice! 
+greet() 
+greet("Alice")  
