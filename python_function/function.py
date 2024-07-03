d2d880143b1dd446f307711f0241ac684bcf1012
osama seid <osaseid1@gmail.com>
2024-07-03 21:39:22 +0300

    area of rectangle

--- python_function/function.py
+++ python_function/function.py
@@ -1,3 +1,8 @@
+length = float(input("Enter the length: "))
+width = float(input("Enter the width: "))
+
 def calculate_area(length, width):
     area = length * width
     return area
+
+print(calculate_area(length, width))
