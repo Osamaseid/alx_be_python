73921aaf19d1e2cf66149a6dea75275e475b8e0a
osama seid <osaseid1@gmail.com>
2024-07-03 10:30:35 +0300

    default function

---
+++ python_function/default_function.py
@@ -0,0 +1,6 @@
+# Function definition with default value
+def greet(name="World"):
+     """Prints a greeting message."""
+     print(f"Hello, {name}!")
+greet()  # Output: Hello, World!
+greet("Alice")  # Output: Hello, Alice! 
