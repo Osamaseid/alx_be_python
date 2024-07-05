b5c2a6177f0fdd6f0220273b2dc0dc553cdf1b21
osama seid <osaseid1@gmail.com>
2024-07-05 15:50:12 +0300

    multiplicacction table

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,5 +1,10 @@
-number = int(input("Enter a number to see its multiplication table: "))
+def main():
 
-for i in range(1, 11):
-    result = number * i
-    print(f"{number} x {i} = {result}")
+    number = int(input("Enter a number to see its multiplication table: "))
+    
+    for i in range(1, 11):
+        product = number * i
+        print(f"{number} * {i} = {product}")
+
+if __name__ == "__main__":
+    main()
