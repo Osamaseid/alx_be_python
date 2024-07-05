eb9128dbfe476b1a170b29a8325cb4d587c4f4e0
osama seid <osaseid1@gmail.com>
2024-07-05 14:43:06 +0300

    global variable

---
+++ local-globalVar/globalvar.py
@@ -0,0 +1,12 @@
+def counting_function():
+    count = 0
+    for i in range(5):
+        count += 1
+    print(f"The count in the counting function is: {count}")
+
+def logging_function():
+    count = "This is a log message."
+    print(f"The count in the logging function is: {count}")
+
+counting_function()
+logging_function()
