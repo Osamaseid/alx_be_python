921c7be354be3f49f515bcca902061b1295b2243
osama seid <osaseid1@gmail.com>
2024-07-05 23:16:53 +0300

    date and time

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,16 @@
+import datetime
+
+def display_current_datetime():
+    current_date = datetime.datetime.now()
+    print(f"Current date and time: {current_date.strftime('%Y-%m-%d %H:%M:%S')}")
+
+def calculate_future_date():
+    
+    num_days = int(input("Enter the number of days to add to the current date: "))
+    current_date = datetime.datetime.now()
+    future_date = current_date + datetime.timedelta(days=num_days)
+    print(f"Future date: {future_date.strftime('%Y-%m-%d')}")
+
+if __name__ == "__main__":
+    display_current_datetime()
+    calculate_future_date()
