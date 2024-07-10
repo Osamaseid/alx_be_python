c283bb4a8fe64f49cc76e4f4b3e323c9934fb56c
osama seid <osaseid1@gmail.com>
2024-07-10 12:48:07 +0300

    value to high error

---
+++ exception/ValueTooHighError.py
@@ -0,0 +1,19 @@
+class ValueTooHighError(Exception):
+    
+    pass
+
+def check_value(num):
+    if num > 100:
+        raise ValueTooHighError(f"The value {num} is too high. The maximum allowed value is 100.")
+    else:
+        print(f"The value {num} is valid.")
+
+try:
+    user_input = int(input("Enter a number: "))
+    check_value(user_input)
+except ValueTooHighError as e:
+    print(e)
+except ValueError:
+    print("Invalid input. Please enter a number.")
+
+    
