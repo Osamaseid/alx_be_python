5e8cd030520600bb9f3bbf9d7a8f70c4dd43e441
osama seid <osaseid1@gmail.com>
2024-07-10 08:08:01 +0300

    exceptios

---
+++ exception/exception.py
@@ -0,0 +1,14 @@
+
+try :
+    f = open("text_file.txt")
+except FileNotFoundError as e :
+    print(e)
+except Exception as e :
+    print(e)
+else :
+    print(f.read())
+    f.close()
+finally :
+    print("nothig is here")
+    
+        
