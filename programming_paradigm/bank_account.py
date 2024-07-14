e7e9367e99282252d3645914eb041bed84c90aa9
osama seid <osaseid1@gmail.com>
2024-07-14 08:44:27 +0300

    simple banck account

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,16 +1,17 @@
-class BankAccount :
-    def __init__(self, account_balance = 0):
-        self.account_balance = account_balance
+
+class BankAccount:
+    def __init__(self, initial_balance=0):
+        self.account_balance = initial_balance
 
     def deposit(self, amount):
         self.account_balance += amount
-        
+
     def withdraw(self, amount):
-        if self.account_balance >= amount:
+        if amount <= self.account_balance:
             self.account_balance -= amount
             return True
         else:
             return False
-    
+
     def display_balance(self):
-      print(f"Current Balance: ${self.account_balance:.2f}")
+        print(f"Current Balance: ${self.account_balance:.2f}")
