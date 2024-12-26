fc9fd23f211e90d0bc8895b8a4026be9821c2f35
osama seid <osaseid1@gmail.com>
2024-12-27 02:05:18 +0300

    decorators-argument

--- Advanced_python/decorator-Argument.py
+++ Advanced_python/decorator-Argument.py
@@ -1,7 +1,7 @@
 def do_twice(func):
     def wrapper_do_twice(*args, **kwargs):
-        fun(*args, **kwargs)
-        fun(*args, **kwargs)
+        func(*args, **kwargs)
+        func(*args, **kwargs)
     return wrappers_do_twice
 @do_twice
 def greet(name):
