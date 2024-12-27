187102933117669d0cc67c0e55f885b4a6c1c3c4
osama seid <osaseid1@gmail.com>
2024-12-27 15:10:44 +0300

    decorators added

--- Advanced_python/decorator-Argument.py
+++ Advanced_python/decorator-Argument.py
@@ -2,7 +2,7 @@ def do_twice(func):
     def wrapper_do_twice(*args, **kwargs):
         func(*args, **kwargs)
         func(*args, **kwargs)
-    return wrappers_do_twice
+  
 @do_twice
 def greet(name):
     print(f"Hello{name}") 
