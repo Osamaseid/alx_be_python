39314a0c3ceb092b7f9faf8568892d2d30663c1f
osama <osaseid1@gmail.com>
2026-01-05 23:25:08 +0300

    control flow

--- python_introduction/hello.py
+++ python_introduction/hello.py
@@ -4,3 +4,13 @@ myName = input()
 print("good to see you" + myName)
 print("the length of your name is :")
 print(len(myName))
+print('What is your age?') # ask for their age
+myAge = input()
+print('You will be ' + str(int(myAge) + 1) + ' in a year.')
+
+if name == 'Mary':
+  print('Hello Mary')
+if password == 'swordfish':
+    print('Access granted.')
+else:
+   print('Wrong password.')
